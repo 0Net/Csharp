b57b75f0027a8ec8c2b6553f97a9676f797406af
0Net <0net@mail.ru>
2018-10-12 11:30:22 +0300

    Реализовано решение задачи #6

--- Lesson-1/Lesson-2/Task6.cs
+++ Lesson-1/Lesson-2/Task6.cs
@@ -12,9 +12,28 @@ namespace Menu
 {
     partial class Program
     {
+        static bool GoodNumber (int number)
+        {
+            return number % Count(number) == 0;
+        }
+
         static void Task6()
         {
-
+            // Count определён в задаче №2
+            Console.Clear();
+            DateTime begin = DateTime.Now;
+            int gnCount = 0;
+            for(int i = 1; i <= 1000000000; i++)
+            {
+                if (GoodNumber(i))
+                {
+                    gnCount++;
+                }
+            }
+            Console.WriteLine($"Количество хороших чисел: {gnCount}");
+            Console.WriteLine();
+            Console.Write("Время выполнения задачи: ");
+            Console.WriteLine(DateTime.Now - begin);
         }
     }
 }
