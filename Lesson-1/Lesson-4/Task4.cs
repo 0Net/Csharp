e5f2c05a6cdca5aee182d593fedf7fafb5f8384a
0Net <0net@mail.ru>
2018-10-19 11:39:51 +0300

    Дописал задания №4 и №5

--- Lesson-1/Lesson-4/Task4.cs
+++ Lesson-1/Lesson-4/Task4.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 /* Задоров Вадим. Домашнее задание №4.
 4. Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив. Создайте структуру Account, содержащую Login и Password.
@@ -11,9 +12,50 @@ namespace Menu
 {
     partial class Program
     {
+        static bool Check(string filename)
+            {
+            int count = 1;
+            string[] lines = File.ReadAllLines(filename);
+            string[,] array = new string[lines.Length, lines[0].Split(';').Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] temp = lines[i].Split(';');
+                for (int j = 0; j < temp.Length; j++)
+                    array[i, j] = temp[j];
+            }
+
+            do
+            {
+                count++;
+                Console.WriteLine("Введите логин");
+                string login = Console.ReadLine();
+                Console.WriteLine("Введите пароль");
+                string password = Console.ReadLine();
+                for(int i = 0; i < lines.Length; i++)
+                { 
+                    if (login == array[i,0] && password == array[i,1])
+                    {
+                        return true;
+                    }
+                }
+            }
+            while (count <= 3);
+            return false;
+    }
+
+
         static void Task4()
         {
             Console.Clear();
+            //Блок создания файла с логинами и паролями. Табуляция через ";"
+            string filename = "auth.txt";
+            StreamWriter sw = null;
+            sw = new StreamWriter(filename);
+            sw.WriteLine("root;GeekBrains");
+            sw.WriteLine("admin; GeekBrains");
+            sw.Close();
+
+            Console.WriteLine(Check(filename));
         }
     }
 }
--- Lesson-1/Lesson-4/Task5.cs
+++ Lesson-1/Lesson-4/Task5.cs
@@ -8,15 +8,112 @@ using System.Threading.Tasks;
 Создать методы, которые возвращают сумму всех элементов массива, сумму всех элементов массива больше заданного, свойство, возвращающее минимальный элемент массива, 
 свойство, возвращающее максимальный элемент массива, метод, возвращающий номер максимального элемента массива(через параметры, используя модификатор ref или out).
   * б) Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
-  ** в) Обработать возможные исключительные ситуации при работе с файлами.
+  ** в) Обработать возможные исключительные ситуации при работе с файлами. 
 */
 namespace Menu
 {
+    class DoubleArray
+    {
+        int[,] a;
+        int rows, lines;
+
+        Random rnd = new Random();
+
+        public DoubleArray(int n, int m)
+        {
+            a = new int[n,m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++) { 
+                a[i,j] = rnd.Next();
+                }
+            }
+            lines = n;
+            rows = m;
+        }
+
+        public double Summ()
+        {
+            double summ = 0;
+            for(int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    summ += a[i, j];
+                }
+            }
+            return summ;
+        }
+
+        public int Min
+        {
+            get
+            {
+                int min = a[0,0];
+                for (int i = 0; i < lines; i++)
+                {
+                    for (int j = 0; j < rows; j++)
+                    {
+                        if (a[i, j] < min) 
+                        {
+                            min = a[i, j];
+                        }
+                    }
+                }
+                return min;
+            }
+        }
+
+        public int Max
+        {
+            get
+            {
+                int max = a[0, 0];
+                for (int i = 0; i < lines; i++)
+                {
+                    for (int j = 0; j < rows; j++)
+                    {
+                        if (a[i, j] >= max)
+                        {
+                            max = a[i, j];
+                        }
+                    }
+                }
+                return max;
+            }
+        }
+
+        public string MaxIndex()
+        {
+            string index = "";
+            int max = a[0, 0];
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    if (a[i, j] >= max)
+                    {
+                        max = a[i, j];
+                        index = i + " " + j;
+                    }
+                }
+            }
+            return index;
+        }
+
+    }
+
     partial class Program
     {
         static void Task5()
         {
             Console.Clear();
+            DoubleArray array = new DoubleArray(3, 5);
+            Console.WriteLine(array.Summ());
+            Console.WriteLine(array.Min);
+            Console.WriteLine(array.Max);
+            Console.WriteLine(array.MaxIndex());
+
         }
     }
 }
