8aca551a07d14f0ac81690cb1ecd30eea6d57d49
0Net <0net@mail.ru>
2018-10-22 15:05:25 +0300

    Выполнил ДЗ №5

---
+++ Lesson-1/Lesson-5/Task3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Задоров Вадим. Домашнее задание №5.
+ 3. *Для двух строк написать метод, определяющий, является ли одна строка перестановкой
+другой.
+Например:
+badc​ являются перестановкой ​abcd​.
+ */
+namespace Lesson_5
+{
+    partial class Program
+    {
+        static void Task3()
+        {
+
+        }
+    }
+}
