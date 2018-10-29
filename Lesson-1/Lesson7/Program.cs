dcd48c82eead7de593a434b1346ba010b2738f6b
0Net <0net@mail.ru>
2018-10-29 15:52:18 +0300

    Выполнено ДЗ №7

---
+++ Lesson-1/Lesson7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+/* Задоров Вадим ДЗ №7
+ 1. а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
+б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число
+должен получить игрок. Игрок должен получить это число за минимальное количество ходов.
+в) *Добавить кнопку «Отменить», которая отменяет последние ходы. Используйте
+обобщенный класс Stack
+*/
+
+namespace Lesson7
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new fMain());
+        }
+    }
+}
